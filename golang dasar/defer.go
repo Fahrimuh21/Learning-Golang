fa4d275a3f58327893a48787a92ea4bf98ce1c1a
Muhammad Fahri <fahrimuh775@gmail.com>
2025-09-16 22:38:37 +0700

    modifikasi defer dan panic

--- golang dasar/defer.go
+++ golang dasar/defer.go
@@ -8,6 +8,7 @@ func logging() {
 
 func runGetApp() {
 	defer logging() // ini buat skip paling akhir program
+	//buat mengatasi eror kalo ada di atasnya
 	fmt.Println("run Application")
 }
 func main() {
--- golang dasar/panic.go
+++ golang dasar/panic.go
@@ -4,17 +4,21 @@ import "fmt"
 
 func endApp() {
 	fmt.Println("end app")
+	message := recover()
+	fmt.Println("Terjadi panic", message)
 }
 
 func runApp(error bool) {
+	defer endApp() //biar masuk endapp
 	if error {
-		panic("ups eror")
-
+		panic("ups eror") //langsung berhenti
 	}
-	endApp()
+
+	// endApp() ini tidak akan dipanggil
 
 }
 
 func main() {
 	runApp(true)
+	fmt.Println("Muhammad Fahri")
 }
